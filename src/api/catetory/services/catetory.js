b9822ec47efc8917f80352aaedebb94daf9f9937
vla0413 <puhachtymur@gmail.com>
2024-06-05 21:18:35 +0300

    update

---
+++ src/api/catetory/services/catetory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catetory service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::catetory.catetory');
---
+++ types/generated/components.d.ts
@@ -0,0 +1,71 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SharedMedia extends Schema.Component {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'cube';
+  };
+  attributes: {
+    file: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+  };
+}
+
+export interface SharedQuote extends Schema.Component {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'cube';
+  };
+  attributes: {
+    title: Attribute.String;
+    body: Attribute.Text;
+  };
+}
+
+export interface SharedRichText extends Schema.Component {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    displayName: 'Rich Text';
+    icon: 'cube';
+  };
+  attributes: {
+    body: Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'Seo';
+    icon: 'cube';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.String;
+    shareImage: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface SharedSlider extends Schema.Component {
+  collectionName: 'components_shared_sliders';
+  info: {
+    displayName: 'Slider';
+    icon: 'cube';
+  };
+  attributes: {
+    files: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+    }
+  }
+}
